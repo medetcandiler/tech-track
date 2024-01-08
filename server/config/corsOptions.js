6512673b6984e54290f13dc469eef835bf1df96d
Medetcan Diler <medetdiler1996@gmail.com>
2024-01-08 20:32:11 +0300

    Updated corsOptions

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -2,12 +2,14 @@ const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
   origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin ) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
-  }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
 }
 
 module.exports = corsOptions;
